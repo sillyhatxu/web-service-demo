3c2c14e842e350309943ea139c74aaa5fb932e21
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2020-09-30 22:53:07 +0800

    initial web service demo

--- application-api/main.go
+++ application-api/main.go
@@ -3,8 +3,5 @@ package main
 import "github.com/sillyhatxu/web-service-demo/application-api/api"
 
 func main() {
-	//os.Setenv("TEST","hello api")
-	//os.Setenv("INTERNAL_HOST","http://localhost:8082")
-	//api.InitialAPI(8081)
 	api.InitialAPI(8080)
 }
